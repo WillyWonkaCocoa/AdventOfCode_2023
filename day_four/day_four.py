29ce12b1976e32d66733b30f7186347bf82b90d3
Will Gao <wgao1@umbc.edu>
2023-12-03 23:25:46 -0800

    clean up print statements

--- day_four/day_four.py
+++ day_four/day_four.py
@@ -7,7 +7,6 @@ def solution(filename):
     f = open(filename) 
     lines = f.readlines()
     cards = np.ones(len(lines)+1, dtype=int)
-    print(cards)
     index = 1
 
     for line in lines:
@@ -19,21 +18,17 @@ def solution(filename):
         winning_nums = [int(x) for x in winning_nums.strip().split()]
         scratched_nums = [int(x) for x in scratched_nums.strip().split()]
         winning_dict = {x:1 for x in winning_nums}
-
         num_of_matches = 0
+
         for num in scratched_nums:
             if num in winning_nums:
                 num_of_matches += 1
+
         for i in range(1, num_of_matches+1):
             cards[index+i] += cards[index]
-            #print("Card {} has {} copies after Card {}".format(index+i, cards[index+i], index))
 
         index += 1
-
-    total_cards = sum(cards[1:])
-    #print(cards)
-    print(total_cards)
-    return total_cards
+    return sum(cards[1:])
 
 def test_sample():
     assert(solution(sample_input) == 30)
