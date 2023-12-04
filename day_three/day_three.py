0c5239fdff4102d3cfa4ae4fdb2fc0a6a46c7004
Will Gao <wgao1@umbc.edu>
2023-12-03 16:24:16 -0800

    day three part one bug fix: account for line ending in digits instead of period

--- day_three/day_three.py
+++ day_three/day_three.py
@@ -1,4 +1,5 @@
 import pytest
+from colored import fg
 
 sample_input = "sample_input.txt"
 
@@ -38,14 +39,14 @@ def solution(filename):
                     x+1 < x_dim and y+1 < y_dim and is_symbol(lines[x+1][y+1]):
                         isAdjacent = True
                     
-            elif not c.isdigit() and is_part_of_num:
+            if (not c.isdigit() or y == y_dim - 1) and is_part_of_num:
                 # symbol or period following a number
                 #print("num is: {}".format(num))
                 # check if adjacent to symbol
                 if isAdjacent:
                     # add to sum
-                    #print("{} is adjacent to a symbol".format(num))
-                    print("{} plus {} = {}".format(int(num), sum, sum+int(num)))
+                    print("{} is adjacent to a symbol".format(num))
+                    #print("{} plus {} = {}".format(int(num), sum, sum+int(num)))
                     sum += int(num)
                     
 
@@ -56,7 +57,23 @@ def solution(filename):
             y += 1
         x += 1
         y = 0
-    print("sum is: {}".format(sum))
+    #print("sum is: {}".format(sum))
+
+    for n in lines:
+        for m in n:
+            color = fg('blue')
+            if m.isdigit():
+                color = fg('green')
+                print(color+m, end =" ")
+            elif is_symbol(m):
+                color = fg('red')
+                print(color+m, end =" ")
+            else:
+                color = fg('white')
+                print(color+m, end =" ")
+        print("\n")
+
+
     return sum
 
 def is_symbol(c):
@@ -66,5 +83,5 @@ def test_sample():
     assert(solution(sample_input) == 4361)
 
 answer = solution('day_three.txt')
-
+print(answer)
 #test_sample()
