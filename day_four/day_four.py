c860c1d2268ef74d32610ec691d5a3cbb88ae1d7
Will Gao <wgao1@umbc.edu>
2023-12-03 23:23:36 -0800

    day four part two

--- day_four/day_four.py
+++ day_four/day_four.py
@@ -1,11 +1,14 @@
 import pytest
+import numpy as np
 
 sample_input = "sample_input.txt"
 
 def solution(filename):
     f = open(filename) 
     lines = f.readlines()
-    sum = 0
+    cards = np.ones(len(lines)+1, dtype=int)
+    print(cards)
+    index = 1
 
     for line in lines:
         start_index = line.find(":")
@@ -17,21 +20,23 @@ def solution(filename):
         scratched_nums = [int(x) for x in scratched_nums.strip().split()]
         winning_dict = {x:1 for x in winning_nums}
 
-        points_for_line = -1
+        num_of_matches = 0
         for num in scratched_nums:
             if num in winning_nums:
-                if points_for_line == -1:
-                    points_for_line = 1
-                else:
-                    points_for_line *= 2
+                num_of_matches += 1
+        for i in range(1, num_of_matches+1):
+            cards[index+i] += cards[index]
+            #print("Card {} has {} copies after Card {}".format(index+i, cards[index+i], index))
 
-        if points_for_line > 0:
-            sum += points_for_line
-    print(sum)
-    return sum
+        index += 1
+
+    total_cards = sum(cards[1:])
+    #print(cards)
+    print(total_cards)
+    return total_cards
 
 def test_sample():
-    assert(solution(sample_input) == 13)
+    assert(solution(sample_input) == 30)
 
 answer = solution('day_four.txt')
 
