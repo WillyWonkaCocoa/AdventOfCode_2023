1987b0d29254f10bcd399314b627d0778d6022a9
Will Gao <wgao1@umbc.edu>
2023-12-03 21:42:22 -0800

    day four part one

---
+++ day_four/day_four.py
@@ -0,0 +1,41 @@
+import pytest
+
+sample_input = "sample_input.txt"
+
+def solution(filename):
+    f = open(filename) 
+    lines = f.readlines()
+    sum = 0
+
+    for line in lines:
+        start_index = line.find(":")
+        card_id = line[5:start_index]
+        print("Card {}".format(card_id))
+        if start_index != -1:
+            # get all games
+            winning_nums, scratched_nums = line[start_index+1:].strip().split("|")
+        winning_nums = [int(x) for x in winning_nums.strip().split()]
+        scratched_nums = [int(x) for x in scratched_nums.strip().split()]
+        winning_dict = {x:1 for x in winning_nums}
+
+        points_for_line = -1
+        for num in scratched_nums:
+            if num in winning_nums:
+                print("Winning number: {}!".format(num))
+                if points_for_line == -1:
+                    points_for_line = 1
+                else:
+                    points_for_line *= 2
+                print("{} point".format(points_for_line))
+
+        if points_for_line > 0:
+            sum += points_for_line
+    print(sum)
+    return sum
+
+def test_sample():
+    assert(solution(sample_input) == 13)
+
+answer = solution('day_four.txt')
+
+#test_sample()
