5f3c24e22d1d72c2687af4060a5d9b840a8d8d6f
Will Gao <wgao1@umbc.edu>
2023-12-05 22:36:32 -0800

    day six part two

--- day_six/day_six.py
+++ day_six/day_six.py
@@ -6,38 +6,26 @@ def solution(filename):
     f = open(filename) 
     lines = f.readlines()
 
-    times = convert_to_list_of_num(lines[0])
-    distances = convert_to_list_of_num(lines[1])
+    total_time = convert_to_list_of_num(lines[0])
+    record_distance = convert_to_list_of_num(lines[1])
+    num_ways_to_win = 0
 
-    if len(times) != len(distances):
-        print("Length of times and distances are not equal!")
-        return 0
+    for hold_time in range(1, total_time):
+        if (total_time - hold_time)*hold_time >record_distance:
+            num_ways_to_win += 1
 
-    index = 0
-    answer = 1
-    for i in range(len(times)):
-        num_ways_to_win = 0
-        total_time = times[index]
-        record_distance = distances[index]
-        for hold_time in range(1, total_time):
-            if (total_time - hold_time)*hold_time >record_distance:
-                num_ways_to_win += 1
-        
-        answer *= num_ways_to_win
-        index += 1
-
-    return answer
+    return num_ways_to_win
 
 def convert_to_list_of_num(lst):
     start_index = lst.find(":")
     if start_index != -1:
         lst = lst[start_index+1:].strip().split()
-        lst = [int(x) for x in lst]
+        lst = int("".join(lst))
 
     return lst
 
 def test_sample():
-    assert(solution(sample_input) == 288)
+    assert(solution(sample_input) == 71503)
 
 answer = solution('day_six.txt')
 
