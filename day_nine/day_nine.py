e65ac352120199c4133438eaadc1a7584d89ba81
Will Gao <wgao1@umbc.edu>
2023-12-08 21:39:29 -0800

    day nine part one

---
+++ day_nine/day_nine.py
@@ -0,0 +1,40 @@
+import pytest
+
+sample_input = "sample_input.txt"
+
+def solution(filename):
+    f = open(filename) 
+    lines = f.readlines()
+    sum = 0
+
+    for line in lines:
+        line = line.strip().split()
+        line = [int(x) for x in line]
+        temp = line[-1] + find_next_in_line(line)
+        print("next in {} is: {}".format(line, temp))
+        sum += temp
+        
+    print(sum)
+    return sum
+
+def find_next_in_line(line):
+    next_num = 0
+    prev_num = line[0]
+    new_line = []
+    for i in range(1, len(line)):
+        temp = line[i] - prev_num
+        prev_num = line[i]
+        new_line.append(temp)
+
+    if len(set(new_line)) == 1:
+        # all the same number
+        return new_line[0]
+    else:
+        return new_line[-1] + find_next_in_line(new_line)
+
+def test_sample():
+    assert(solution(sample_input) == 114)
+
+answer = solution('day_nine.txt')
+
+#test_sample()
