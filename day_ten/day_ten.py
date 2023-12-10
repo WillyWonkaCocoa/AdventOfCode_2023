8f0d56cfbf6a76ca0b41292547f7f5d51fccf44a
Will Gao <wgao1@umbc.edu>
2023-12-09 23:30:33 -0800

    clean up print statements

--- day_ten/day_ten.py
+++ day_ten/day_ten.py
@@ -1,5 +1,4 @@
 import pytest
-
 sample_input = "sample_input.txt"
 sample_input_two = "sample_input_two.txt"
 
@@ -18,9 +17,6 @@ def solution(filename):
             s_location = (x_index, map[x_index].index("S"))
         x_index += 1
 
-    
-        
-    print("s_location: {}".format(s_location, map))
     max_steps = find_len_of_path(s_location, map) // 2
     print(max_steps)
     return max_steps
@@ -29,11 +25,8 @@ def find_len_of_path(s_coord, map):
     total_steps = 1
     prev_coord = s_coord
     curr_coord = find_connecting_pipe(s_coord, s_coord, map)
-    print("curr_coord: {} prev_coord: {}".format(s_coord, prev_coord))
     while(map[curr_coord[0]][curr_coord[1]] != "S"):
         tmp = curr_coord
-        print("curr_ pipe: {}".format(map[curr_coord[0]][curr_coord[1]]))
-        print("curr_coord: {} prev_coord: {}".format(curr_coord, prev_coord))
         curr_coord = find_connecting_pipe(curr_coord, prev_coord, map)
         prev_coord = tmp
         total_steps += 1
@@ -60,30 +53,27 @@ def find_connecting_pipe(coord, prev_coord, map):
     x_lim = len(map) - 1
     y_lim = len(map[0]) - 1
     x_coord, y_coord, = coord[0], coord[1]
-    
     directions_to_check = pipe_piece_map[map[x_coord][y_coord]]
-    print(directions_to_check)
 
     for dir in directions_to_check:
-        print("dir: {}".format(dir))
         if dir == "TOP" and x_coord - 1 >=  0 and \
             not (x_coord - 1 == prev_coord[0] and y_coord == prev_coord[1]):
-            print("checking top")
+
             if map[x_coord - 1][y_coord] in direction_to_piece_map["TOP"]:
                 return (x_coord - 1,y_coord)
         elif dir == "BOTTOM" and x_coord + 1 <= x_lim and \
             not (x_coord + 1 == prev_coord[0] and y_coord == prev_coord[1]):
-            print("checking bottom")
+
             if map[x_coord + 1][y_coord] in direction_to_piece_map["BOTTOM"]:
                 return (x_coord + 1,y_coord)
         elif dir == "LEFT" and y_coord - 1 >= 0 and \
             not (x_coord == prev_coord[0] and y_coord - 1 == prev_coord[1]):
-            print("checking left")
+
             if map[x_coord][y_coord - 1] in direction_to_piece_map["LEFT"]:
                 return (x_coord,y_coord-1)
         elif dir == "RIGHT" and y_coord + 1 <= y_lim and \
             not (x_coord == prev_coord[0] and y_coord + 1 == prev_coord[1]): 
-            print("checking right")
+
             if map[x_coord][y_coord + 1] in direction_to_piece_map["RIGHT"]:
                 return (x_coord,y_coord+1)
 
